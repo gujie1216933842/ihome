a870cd92b18a86ea627f7d94df9501b704e524d7
gujie1216933842 <gujientsy@163.com>
2017-11-14 08:02:31 +0800

    ihome

--- handlers/Passport.py
+++ handlers/Passport.py
@@ -1,7 +1,14 @@
 # Author:Bob
 from .BaseHandler import BaseHandler
-
+import logging
 
 class IndexHandler(BaseHandler):
     def get(self):
+
+        logging.debug('dug msg')
+        logging.info('info msg')
+        logging.warning('warning msg')
+        logging.error('error msg')
+
+
         self.write('欢迎来到tornado的世界!')
