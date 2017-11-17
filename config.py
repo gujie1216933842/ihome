ba345ea9ad869229f97ff11c923ed385593aefcc
gujie1216933842 <gujientsy@163.com>
2017-11-18 00:45:21 +0800

    ihome

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 # Application配置
 setting = {
     'static_path': os.path.join(os.path.dirname(__file__), 'static'),
-    'template_path': os.path.join(os.path.dirname(__file__), 'template'),
+    'template_path': os.path.join(os.path.dirname(__file__), 'html'),
     'cookie_secret': 'ca/woTXGTRmbgilfRjtsB56tNpLoPUqesEPqHD1L0vE=',
     'xsrf_cookies': '+IMtKSeQQTi7YjIOl/oZjXopKd16E0erj9Llvbp7KQQ=',
     'debug': True,
