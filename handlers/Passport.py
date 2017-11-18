be827bb2b04dc170417373481552f101d1490767
gujie1216933842 <gujientsy@163.com>
2017-11-18 19:51:07 +0800

    ihome

--- handlers/Passport.py
+++ handlers/Passport.py
@@ -10,5 +10,5 @@ class IndexHandler(BaseHandler):
         logging.warning('warning msg')
         logging.error('error msg')
 
-
+        self.render()
         self.write('欢迎来到tornado的世界!')
--- urls.py
+++ urls.py
@@ -1,10 +1,13 @@
-from handlers import Passport,IndexHandler
+from handlers import Passport, IndexHandler
 from config import setting
+import os
 
 from handlers.BaseHandler import StaticFileBaseHandler as StaticFileHandler
-
 
 urls = [
     (r"/", Passport.IndexHandler),
     (r"/index", IndexHandler.Indexhandler),
+
+    (r"/(.*)", StaticFileHandler,
+    dict(path=os.path.join(os.path.dirname(__file__), "html"), default_filename="index.html"))
 ]
