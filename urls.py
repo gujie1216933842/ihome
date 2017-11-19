a38981a46e8bcdafc355729ef7211440f064af17
gujie1216933842 <gujientsy@163.com>
2017-11-19 17:01:40 +0800

    ihome

--- urls.py
+++ urls.py
@@ -4,11 +4,14 @@ import os
 
 from handlers.BaseHandler import StaticFileBaseHandler as StaticFileHandler
 
+from utils.DIY_captcha import DIY_captcha
+
 urls = [
     (r"/", Passport.IndexHandler),
     (r"/index", House.Indexhandler),
     (r"/register", RegisterHandler.RegisterHandler),
     (r"/api/imagecode", VerifyCode.ImageCodeHandler), #注册页面的验证码的路由
+    (r"/api/new_imagecode", DIY_captcha.DIY_Verifycode), #注册页面的验证码的路由
 
 
 
---
+++ utils/DIY_captcha/DIY_captcha.py
@@ -0,0 +1,14 @@
+from PIL import Image,ImageDraw,ImageFont,ImageFilter
+
+import random
+from handlers.BaseHandler import BaseHandler
+class DIY_Verifycode(BaseHandler):
+    #随机码
+    def get(self,length = 6):
+        code = ''
+        char = '23456789abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
+        for i in range(length):
+            index = random.randint(0,63)
+            # code += char[index]
+
+        self.write(index)
