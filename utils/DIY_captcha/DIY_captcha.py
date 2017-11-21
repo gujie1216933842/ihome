f152e2b5fba04be0f23d3a8897186df05f93d3e7
gujie1216933842 <gujientsy@163.com>
2017-11-21 23:17:46 +0800

    ihome

--- utils/DIY_captcha/DIY_captcha.py
+++ utils/DIY_captcha/DIY_captcha.py
@@ -1,14 +1,43 @@
-from PIL import Image,ImageDraw,ImageFont,ImageFilter
-
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
 import random
 from handlers.BaseHandler import BaseHandler
+
+
 class DIY_Verifycode(BaseHandler):
-    #随机码
-    def get(self,length = 6):
+    '''
+    生成随机码
+    '''
+    def randon_code(self, length=6):
         code = ''
         char = '23456789abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
         for i in range(length):
-            index = random.randint(0,55)
+            index = random.randint(0, 55)
             code += char[index]
 
         self.write(code)
+    '''
+    随机颜色
+    '''
+    def randon_color(self):
+        return (random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))
+
+    # 生成图片
+    def veri_code(self, width=160, height=40):
+        # 创建image对象
+        image = Image.new('RBG', (width, height), (255, 255, 0))
+        # 创建font对象
+        font = ImageFont.truetype('Arial.ttf', 32)
+        # 创建画布对象
+        draw = ImageDraw.Draw(image)
+        # 随机颜色填充每一个像素
+        for x in range(width):
+            for y in range(height):
+                draw.point((x, y), fill=random(64, 255))
+        # 验证码
+        code = self.randon_code()
+        # 随机颜色验证码写到图片上
+        for t in range(6):
+            draw.text((40 * t + 5, 5), code[t], font=font, fill=self.randon_color())
+        # 模糊滤镜
+        image = image.filter(ImageFilter.BLUR)
+        return code, image
