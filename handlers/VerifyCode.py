18c0017a48a8cf942f0fe25fbdd3fd9f78526953
gujie1216933842 <gujientsy@163.com>
2017-12-06 00:08:56 +0800

    ihome

--- handlers/VerifyCode.py
+++ handlers/VerifyCode.py
@@ -3,7 +3,7 @@ import logging
 from utils.captcha import captcha
 from .BaseHandler import BaseHandler
 import constant
-from utils.DIY_captcha import DIY_captcha
+from utils.DIY_captcha.DIY_captcha import DIY_Verifycode
 
 
 class ImageCodeHandler(BaseHandler):
@@ -21,7 +21,7 @@ class ImageCodeHandler(BaseHandler):
                 # name:验证码名称
                 # text:验证码内容
                 # image:验证码二进制数据
-        code, image = DIY_captcha.DIY_Verifycode().get()
+        code, image = DIY_Verifycode.get()
         logging.info('验证码code_lower:' + code)
         # 存入redis
         try:
