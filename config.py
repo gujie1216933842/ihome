7bd84a815424f1a07ac55a7d1285691f21bebdc2
gujie1216933842 <gujientsy@163.com>
2017-12-17 15:50:40 +0800

    ihome

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ setting = {
     'static_path': os.path.join(os.path.dirname(__file__), 'static'),
     'template_path': os.path.join(os.path.dirname(__file__), 'html'),
     'cookie_secret': 'ca/woTXGTRmbgilfRjtsB56tNpLoPUqesEPqHD1L0vE=',
-    'xsrf_cookies': '+IMtKSeQQTi7YjIOl/oZjXopKd16E0erj9Llvbp7KQQ=',
+    'xsrf_cookies': True,
     'debug': True,
 }
 
