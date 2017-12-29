691a19996fb79a452d8ce534248c024d08c5afd4
gujie1216933842 <gujientsy@163.com>
2017-12-29 17:40:15 +0800

    ihome

--- handlers/VerifyCode.py
+++ handlers/VerifyCode.py
@@ -84,7 +84,7 @@ class regiser(BaseHandler):
         #把输入的图片验证码都变成小写
         lower_picocde = ''
         for i in range(len(imagecode)):
-            lower_picocde += imagecode.decode()[i]
+            lower_picocde += imagecode[i]
 
         logging.info('输入的小写验证码:'+lower_picocde)
         logging.info('redis中的验证码'+imagecode)
