867935191abe8a92ede630f67bc7162db6f6958f
gujie1216933842 <gujientsy@163.com>
2017-12-31 17:06:58 +0800

    ihome_pycharm

--- handlers/Passport.py
+++ handlers/Passport.py
@@ -2,12 +2,22 @@
 from .BaseHandler import BaseHandler
 import logging
 
+
 class IndexHandler(BaseHandler):
     def get(self):
-
         # logging.debug('dug msg')
         # logging.info('info msg')
         # logging.warning('warning msg')
         logging.error('error msg')
 
         self.write('欢迎来到tornado的世界!')
+
+
+class CheckLoginHandler(BaseHandler):
+    def get(self):
+        if not self.get_current_user():
+            return self.write(dict(code='01', msg='false'))
+        else:
+            return self.write(dict(code='00', msg="true"))
+
+
