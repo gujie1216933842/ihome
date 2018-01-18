431f72f0ca1549eb39a4601223516af0b4b538a0
gujientsy <gujientsy@163.com>
2018-01-18 16:39:54 +0800

    ssssssssssssss
    Signed-off-by: gujientsy <gujientsy@163.com>

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ setting = {
 
 # mysql
 mysql_options = dict(
-    host="47.94.165.75",
+    host="47.97.165.75",
     database="ihome",
     user='root',
     password='123',
@@ -19,7 +19,7 @@ mysql_options = dict(
 
 # redis
 redis_options = dict(
-    host='47.94.165.75',
+    host='47.97.165.75',
     port=6379,
 )
 
