ff03563a66c0a500015ce8d996fadac6a755a636
gujie1216933842 <gujientsy@163.com>
2018-01-29 22:52:57 +0800

    ihome

--- handlers/ProfileHandler.py
+++ handlers/ProfileHandler.py
@@ -51,9 +51,7 @@ class UcenterHander(BaseHandler):
             logging.error(e)
             return self.write(dict(code='01', msg='查询数据库出错!'))
         if ret['up_avatar']:
-            logging.info('七牛:' + config.qiniu_url)
             img_url = config.qiniu_url + ret['up_avatar']
-            logging.info('img_url:' + img_url)
         else:
             img_url = None
         self.write(dict(code='00', msg='ok', data=dict(user_id=user_id, name=ret['up_mobile']
