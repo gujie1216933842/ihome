872b0e6c724facb1ee6d624fbe63e320927719ac
gujie1216933842 <gujientsy@163.com>
2018-02-02 23:02:49 +0800

    ihome

--- handlers/ProfileHandler.py
+++ handlers/ProfileHandler.py
@@ -58,6 +58,7 @@ class UcenterHander(BaseHandler):
         self.write(dict(code='00', msg='ok', data=dict(user_id=user_id, name=ret['up_mobile']
                                                        , mobile=ret['up_mobile'], avatar=img_url)))
 
+
 class ProfileShowEdit(BaseHandler):
     @require_logined
     def get(self):
@@ -68,7 +69,7 @@ class ProfileShowEdit(BaseHandler):
         userinfo_data = self.session.data
         logging.info('session中的用户信息:')
         logging.info(userinfo_data)
-        return self.write(dict(code='00',msg='ok',data=userinfo_data))
+        return self.write(dict(code='00', msg='ok', data=userinfo_data))
 
 
 class UploadHandler(BaseHandler):
@@ -76,6 +77,25 @@ class UploadHandler(BaseHandler):
         '''
         头像上传接口
         '''
-
-
-
+        # 接受上传的数据
+        try:
+            image_data = self.request.files[0]['avatar'][0]['body']
+        except Exception as e:
+            logging.error(e)
+            return self.write(dict(code='bb', msg='前端向后台传输图片失败'))
+        # 如果后台接收到图片数据,把图片数据作为参数传递给封装好的七牛接口
+        try:
+            key = storage(image_data)
+        except Exception as e:
+            logging.error(e)
+            return self.write(dict(code='cc', msg="向七牛传递数据出错"))
+        # 七牛上传图片成功,拿到返回的key,把key保存到数据库
+        user_id = self.session.data['user_id']
+        sql = "update ih_user_profile set up_avatar = %(avatar)s where up_user_id = %(user_id)s "
+        try:
+            self.db.excute_rowcount(sql, avatar=key, user_id=user_id)
+        except Exception as e:
+            logging.error(e)
+            return self.write(dict(code="dd", msg="更新数据库图片失败"))
+        # 保存成功
+        return self.write(dict(code="00", msg="ok", data="%s%s" % (config.qiniu_url, key)))
