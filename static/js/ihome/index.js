2987a35113e8f13dbd5bdcf6e94ce3c82339d921
gujie1216933842 <gujientsy@163.com>
2018-02-14 21:24:46 +0800

    Revert "html"
    This reverts commit 75c77809cf277afbc07230985a92c710cfaa759b.

--- static/js/ihome/index.js
+++ static/js/ihome/index.js
@@ -1,11 +1,11 @@
 //模态框居中的控制
-function centerModals() {
-    $('.modal').each(function (i) {   //遍历每一个模态框
-        var $clone = $(this).clone().css('display', 'block').appendTo('body');
+function centerModals(){
+    $('.modal').each(function(i){   //遍历每一个模态框
+        var $clone = $(this).clone().css('display', 'block').appendTo('body');    
         var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2);
         top = top > 0 ? top : 0;
         $clone.remove();
-        $(this).find('.modal-content').css("margin-top", top - 30);  //修正原先已经有的30个像素
+        $(this).find('.modal-content').css("margin-top", top-30);  //修正原先已经有的30个像素
     });
 }
 
@@ -24,7 +24,7 @@ function setStartDate() {
             startDate: startDate,
             format: "yyyy-mm-dd"
         });
-        $("#end-date").on("changeDate", function () {
+        $("#end-date").on("changeDate", function() {
             $("#end-date-input").val(
                 $(this).datepicker("getFormattedDate")
             );
@@ -48,7 +48,7 @@ function goToSearchPage(th) {
     url += ("aid=" + $(th).attr("area-id"));
     url += "&";
     var areaName = $(th).attr("area-name");
-    if (undefined == areaName) areaName = "";
+    if (undefined == areaName) areaName="";
     url += ("aname=" + areaName);
     url += "&";
     url += ("sd=" + $(th).attr("start-date"));
@@ -57,8 +57,8 @@ function goToSearchPage(th) {
     location.href = url;
 }
 
-$(document).ready(function () {
-    $.get("/checklogin", function (data) {
+$(document).ready(function(){
+    $.get("/checklogin", function(data) {
         if ("00" == data.code) {
             $(".top-bar>.user-info>.user-name").html(data.data.name);
             $(".top-bar>.user-info").show();
@@ -66,25 +66,25 @@ $(document).ready(function () {
             $(".top-bar>.register-login").show();
         }
     }, "json");
-    $.get("/house/index", function (data) {
+    $.get("/house/index", function(data){
         if ("00" == data.code) {
-            $(".swiper-wrapper").html(template("swiper-houses-tmpl", {houses: data.houses}));
-            $(".area-list").html(template("area-list-tmpl", {areas: data.areas}));
-            var mySwiper = new Swiper('.swiper-container', {
+            $(".swiper-wrapper").html(template("swiper-houses-tmpl", {houses:data.houses}));
+            $(".area-list").html(template("area-list-tmpl", {areas:data.areas}));
+            var mySwiper = new Swiper ('.swiper-container', {
                 loop: true,
                 autoplay: 2000,
                 autoplayDisableOnInteraction: false,
                 pagination: '.swiper-pagination',
                 paginationClickable: true
             });
-            $(".area-list a").click(function (e) {
+            $(".area-list a").click(function(e){
                 $("#area-btn").html($(this).html());
                 $(".search-btn").attr("area-id", $(this).attr("area-id"));
                 $(".search-btn").attr("area-name", $(this).html());
                 $("#area-modal").modal("hide");
             });
         }
-    }, "json");
+    });
     $('.modal').on('show.bs.modal', centerModals);      //当模态框出现的时候
     $(window).on('resize', centerModals);               //当窗口大小变化的时候
     $("#start-date").datepicker({
@@ -93,7 +93,7 @@ $(document).ready(function () {
         startDate: "today",
         format: "yyyy-mm-dd"
     });
-    $("#start-date").on("changeDate", function () {
+    $("#start-date").on("changeDate", function() {
         var date = $(this).datepicker("getFormattedDate");
         $("#start-date-input").val(date);
     });
