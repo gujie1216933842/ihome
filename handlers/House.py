10755c337afb6cee7656737c28a17f8595535397
gujie1216933842 <gujientsy@163.com>
2018-02-19 12:13:52 +0800

    ihome

--- handlers/House.py
+++ handlers/House.py
@@ -52,7 +52,7 @@ class AreaInfoHandler(BaseHandler):
         # 如果能取到数据(数据存在)
         if ret:
             logging.info(" hit redis: area_info ")
-            resp = '{"code":"0", "msg":"OK", "data":%s}' % ret
+            resp = '{"code":"00", "msg":"OK", "data":%s}' % ret
             return self.write(resp)
 
         # 如果redis中数据为空,需要去数据库中去取
