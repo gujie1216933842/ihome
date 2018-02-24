b744ee6d289e629a3ce7e5537dd5cf40ebb2ab99
gujientsy <gujientsy@163.com>
2018-02-24 11:36:44 +0800

    html

--- static/js/ihome/detail.js
+++ static/js/ihome/detail.js
@@ -13,8 +13,8 @@ function decodeQuery(){
 
 $(document).ready(function(){
     var house_id = decodeQuery()["id"];
-    $.get("/api/house/info?house_id="+house_id, function (data) {
-        if ("0" == data.errcode) {
+    $.get("/house/info?house_id="+house_id, function (data) {
+        if ("00" == data.code) {
             $(".swiper-container").html(template("house-image-tmpl", {"img_urls":data.data.images, "price":data.data.price}));
             $(".detail-con").html(template("house-detail-tmpl", {"house":data.data}));
             var mySwiper = new Swiper ('.swiper-container', {
@@ -23,7 +23,7 @@ $(document).ready(function(){
                 autoplayDisableOnInteraction: false,
                 pagination: '.swiper-pagination',
                 paginationType: 'fraction'
-            })
+            });
             // data.user_id为访问页面用户,data.data.user_id为房东
             if (data.user_id != data.data.user_id) {
                 $(".book-house").attr("href", "/booking.html?hid="+house_id);
@@ -31,4 +31,4 @@ $(document).ready(function(){
             }
         }
     }, "json")
-})
+});
