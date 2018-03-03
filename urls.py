afae6441ef8bfeee0212d057e59126af185a1d19
gujie1216933842 <gujientsy@163.com>
2018-03-03 17:25:41 +0800

    ihome

--- urls.py
+++ urls.py
@@ -1,5 +1,4 @@
 from handlers import Passport, IndexHandler,House,RegisterHandler,VerifyCode,ProfileHandler,Orders
-from config import setting
 import os
 from handlers.BaseHandler import StaticFileBaseHandler as StaticFileHandler
 from utils.DIY_captcha import DIY_captcha
@@ -25,8 +24,8 @@ urls = [
     (r"/tologin", IndexHandler.ToLoginHandler),
     (r"/register_new", VerifyCode.regiser),
     (r"/checklogin",ProfileHandler.ProfileHandler ),
-    (r"/imagecode", VerifyCode.ImageCodeHandler), #注册页面的验证码的路由
-    #(r"/api/new_imagecode", DIY_captcha.DIY_Verifycode), #注册页面的验证码的路由
+    (r"/imagecode", VerifyCode.ImageCodeHandler), #注册页面的验证码的接口
+    #(r"/api/new_imagecode", DIY_captcha.DIY_Verifycode), #单独测试的图片验证码的接口
     (r"/profile/ucenter", ProfileHandler.UcenterHander), #个人中心路由
     (r"/profile/ProfileShowEdit", ProfileHandler.ProfileShowEdit), #修改用户头像和用户名初始化页面显示接口
     (r"/profile/UploadHandler", ProfileHandler.UploadHandler), #用户修改头像上传图片接口
