effd683bee7db08cf4df4d4b871a40f4b96bbd92
gujie1216933842 <gujientsy@163.com>
2018-03-03 17:51:06 +0800

    ihome

--- urls.py
+++ urls.py
@@ -19,8 +19,6 @@ urls = [
     (r'/order/reject', Orders.RejectOrderHandler), # 拒单
     (r'/order/comment', Orders.OrderCommentHandler),
 
-    #(r"/register", RegisterHandler.RegisterHandler),
-    (r"/login", IndexHandler.LoginHandler),
     (r"/tologin", IndexHandler.ToLoginHandler),
     (r"/register_new", VerifyCode.regiser),
     (r"/checklogin",ProfileHandler.ProfileHandler ),
