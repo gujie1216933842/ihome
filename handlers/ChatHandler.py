6eeb3346a4a22a0745ed6d9f038aa66db0e7314d
gujie1216933842 <gujientsy@163.com>
2018-05-16 22:48:50 +0800

    chat

---
+++ handlers/ChatHandler.py
@@ -0,0 +1,17 @@
+
+from .BaseHandler import BaseHandler
+import logging
+import config
+import json
+from utils.common import require_logined
+from utils.image_storage import storage
+import datetime
+import math
+import constants
+from utils import session
+
+class ChatHandler(BaseHandler):
+    def get(self):
+        pass
+
+
