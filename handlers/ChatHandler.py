0aefac28be3d8bac412c22021cdac0cc6ed5a422
gujie1216933842 <gujientsy@163.com>
2018-05-16 23:49:46 +0800

    chat

--- handlers/ChatHandler.py
+++ handlers/ChatHandler.py
@@ -1,47 +1,32 @@
 from tornado.websocket import WebSocketHandler
 from .BaseHandler import BaseHandler
+import time
 
 
 class ShowChatHandler(BaseHandler):
     def get(self):
         self.render('chat.html')
 
+
 class ChatHandler(WebSocketHandler):
     users = []
+
     def open(self):
         for users in self.users:
-            users.write_message("%s上线了"%self.request.remote_ip)
+            users.write_message("[%s]-[%s]-进入聊天室" % (
+            self.request.remote_ip, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))))
         self.users.append(self)
 
-    def on_message(self,msg):
+    def on_message(self, msg):
         for users in self.users:
-            users.write_message("%s说:%s" % (self.request.remote_ip,msg))
+            users.write_message("[%s]-[%s]-说: %s" % (
+                self.request.remote_ip, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), msg))
 
     def on_close(self):
         self.users.remove(self)
         for users in self.users:
-            users.write_message("%s下线了" % self.request.remote_ip)
+            users.write_message("[%s]-[%s]-离开聊天室" % (
+            self.request.remote_ip, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))))
 
-    def check_origin(self,origin):
-        return True  #允许Websocket的跨域请求
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def check_origin(self, origin):
+        return True  # 允许Websocket的跨域请求
